97d9edddf275f8016d0846762742662ea91001e4
arnaud <a.parseval@gmail.com>
2016-01-22 04:37:28 -0800

    avancement formation

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php
 
-function chargerClasse($classe)
+class MaClasse
+
 {
-	require $classe . '.php'; // On inclut la classe correspondante au param�tre pass�.
+
+private $unAttributPrive;
+
+public function __get($nom)
+
+{
+
+return 'Impossible d\'accéder à l\'attribut <strong>' . $nom . '</strong>, désolé !<br />';
+
 }
 
+}
 
-spl_autoload_register('chargerClasse'); // On enregistre la fonction en autoload pour qu'elle soit appel�e d�s qu'on instanciera une classe non d�clar�e.
+$obj = new MaClasse;
 
-//appelle le constructeur du personnage, ce dernier hydrate l'objet
-$perso = new Personnage([
-	'nom' => 'Victor',
-	'forcePerso' => 5,
-	'degats' => 0,
-	'niveau' => 1,
-	'experience' => 0
-]);
+echo $obj->attribut;
 
-//recupère la connection pour la passer au manager
-$db = new PDO('mysql:host=localhost;dbname=formation-php', 'root', '25041981');
-$manager = new PersonnageManager($db);
-//persiste le personnage
-$manager->add($perso);
+echo $obj->unAttributPrive;
